eb198a6e32e2fefc71ff447c9bd0d70adb9b8109
Damien Erambert <damien.erambert@me.com>
2020-05-07 21:05:16 -0700

    support configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import findLastIndex = require('lodash.findlastindex');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const importsRE = new RegExp('^import', 'i');
+const eolToString: { [k in vscode.EndOfLine]: string } = {
+  [vscode.EndOfLine.CRLF]: '\r\n',
+  [vscode.EndOfLine.LF]: '\n',
+};
+
 export function activate() {
-  vscode.workspace.onDidOpenTextDocument((textDoc) => {
-    if (textDoc.languageId === 'typescriptreact') {
-      const firstLine = textDoc.lineAt(0);
-      const textOnFirstLine = firstLine.text;
-      console.log({ textOnFirstLine });
-      if (!textOnFirstLine.startsWith('import')) {
+  const configuration = vscode.workspace.getConfiguration('fold-imports');
+  const importsLanguages: string[] = [
+    'typescriptreact',
+    'typescript',
+    'javascript',
+    'javascriptreact',
+    ...(configuration.extraLanguages || []),
+  ];
+  const { minimumBlockSize } = configuration;
+
+  const logger = (...args: any[]) => {
+    if (configuration.debug) {
+      console.log(...args);
+    }
+  };
+
+  vscode.workspace.onDidOpenTextDocument((document) => {
+    if (importsLanguages.includes(document.languageId)) {
+      // Get the lines of text of the document.
+      const lines = document.getText().split(eolToString[document.eol]);
+      // Find the first line with an import statement.
+      const importLineNumber = lines.findIndex((line) =>
+        Boolean(line.trim().match(importsRE))
+      );
+      const lastLineWithImports = findLastIndex(lines, (line) =>
+        Boolean(line.trim().match(importsRE))
+      );
+
+      // If there is no import statement, nothing to do.
+      if (importLineNumber === -1) {
         return;
       }
 
-      console.log('Gonna fold at the top');
+      // If the import block is smaller than the defined minimum size, nothing to do.
+      if (lastLineWithImports - importLineNumber < minimumBlockSize) {
+        return;
+      }
 
+      logger('Gonna fold at line: ' + importLineNumber);
+      // Fold at the found line number
       vscode.commands.executeCommand('editor.fold', {
         levels: 1,
-        selectionLines: [0],
+        selectionLines: [importLineNumber],
       });
     }
   });
