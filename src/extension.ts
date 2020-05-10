b449d4c810d2186daa7b8884a556a2716642b1a2
Damien Erambert <damien.erambert@me.com>
2020-05-09 18:27:23 -0700

    Add commands to fold/unfold imports

--- src/extension.ts
+++ src/extension.ts
@@ -8,54 +8,139 @@ const eolToString: { [k in vscode.EndOfLine]: string } = {
 };
 const extensionKey = 'auto-fold-imports';
 
-export function activate() {
-  const configuration = vscode.workspace.getConfiguration(extensionKey);
-  const importsLanguages: string[] = [
-    'typescriptreact',
-    'typescript',
-    'javascript',
-    'javascriptreact',
-    ...(configuration.extraLanguages || []),
-  ];
-  const { minimumBlockSize } = configuration;
+interface FoldLineProps {
+  start: number;
+  end: number;
+  size: number;
+}
 
-  const logger = (...args: any[]) => {
-    if (configuration.debug) {
-      console.log(...args);
+enum FoldActions {
+  FOLD = 1,
+  UNFOLD = 2,
+}
+
+const configuration = vscode.workspace.getConfiguration(extensionKey);
+const importsLanguages: string[] = [
+  'typescriptreact',
+  'typescript',
+  'javascript',
+  'javascriptreact',
+  ...(configuration.extraLanguages || []),
+];
+const { minimumBlockSize } = configuration;
+
+const logger = (...args: any[]) => {
+  if (configuration.debug) {
+    console.log(...args);
+  }
+};
+
+export function activate(extensionContext: vscode.ExtensionContext) {
+  const foldCommandDisposable = vscode.commands.registerCommand(
+    'auto-fold-imports.fold',
+    () => {
+      if (!vscode.window.activeTextEditor) {
+        return;
+      }
+
+      const { document } = vscode.window.activeTextEditor;
+      const importLines = findImportLines(document);
+
+      if (!importLines) {
+        return;
+      }
+
+      changeFoldingOfImportLines(FoldActions.FOLD, importLines);
     }
-  };
+  );
+
+  const unfoldCommandDisposable = vscode.commands.registerCommand(
+    'auto-fold-imports.unfold',
+    () => {
+      if (!vscode.window.activeTextEditor) {
+        return;
+      }
+
+      const { document } = vscode.window.activeTextEditor;
+      const importLines = findImportLines(document);
+
+      if (!importLines) {
+        return;
+      }
+
+      changeFoldingOfImportLines(FoldActions.UNFOLD, importLines);
+    }
+  );
+
+  extensionContext.subscriptions.push(foldCommandDisposable);
+  extensionContext.subscriptions.push(unfoldCommandDisposable);
 
   vscode.workspace.onDidOpenTextDocument((document) => {
-    if (importsLanguages.includes(document.languageId)) {
-      // Get the lines of text of the document.
-      const lines = document.getText().split(eolToString[document.eol]);
-      // Find the first line with an import statement.
-      const importLineNumber = lines.findIndex((line) =>
-        Boolean(line.trim().match(importsRE))
-      );
-      const lastLineWithImports = findLastIndex(lines, (line) =>
-        Boolean(line.trim().match(importsRE))
-      );
+    const importLines = findImportLines(document);
 
-      // If there is no import statement, nothing to do.
-      if (importLineNumber === -1) {
-        return;
-      }
-
-      // If the import block is smaller than the defined minimum size, nothing to do.
-      if (lastLineWithImports - importLineNumber < minimumBlockSize) {
-        return;
-      }
-
-      logger('Gonna fold at line: ' + importLineNumber);
-      // Fold at the found line number
-      vscode.commands.executeCommand('editor.fold', {
-        levels: 1,
-        selectionLines: [importLineNumber],
-      });
+    if (!importLines || !shouldFoldImports(importLines)) {
+      return;
     }
+
+    changeFoldingOfImportLines(FoldActions.FOLD, importLines);
   });
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+/**
+ * Helpers.
+ */
+
+function findImportLines(
+  document: vscode.TextDocument
+): FoldLineProps | undefined {
+  if (!importsLanguages.includes(document.languageId)) {
+    return undefined;
+  }
+
+  // Get the lines of text of the document.
+  const lines = document.getText().split(eolToString[document.eol]);
+  // Find the first line with an import statement.
+  const importLineNumber = lines.findIndex((line) =>
+    Boolean(line.trim().match(importsRE))
+  );
+  const lastLineWithImports = findLastIndex(lines, (line) =>
+    Boolean(line.trim().match(importsRE))
+  );
+
+  return {
+    start: importLineNumber,
+    end: lastLineWithImports,
+    size: lastLineWithImports - importLineNumber,
+  };
+}
+
+function changeFoldingOfImportLines(action: FoldActions, lines: FoldLineProps) {
+  logger('Gonna fold at line: ' + lines.start);
+  // Fold at the found line number
+  const command = action === FoldActions.FOLD ? 'editor.fold' : 'editor.unfold';
+  vscode.commands.executeCommand(command, {
+    levels: 1,
+    selectionLines: [lines.start],
+  });
+}
+
+function shouldFoldImports(lines: FoldLineProps | undefined) {
+  if (!lines) {
+    return false;
+  }
+
+  // If there is no import statement, nothing to do.
+  if (lines.start === -1) {
+    return false;
+  }
+
+  // If the import block is smaller than the defined minimum size, nothing to do.
+  if (lines.size < minimumBlockSize) {
+    return false;
+  }
+
+  return true;
+}
